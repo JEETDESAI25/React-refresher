e9dce436ac822510afc6c8d6a65e4a730547208a
JEETDESAI25 <jeetdesai25@gmail.com>
2023-02-27 20:31:51 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import GoalList from "./components/GoalList/GoalList";
 import NewGoal from "./components/NewGoal/NewGoal.js";
 import "./App.css";
 
 const App = () => {
-  const courseGoals = [
+  const [courseGoals, setCourseGoals] = useState([
     { id: "cg1", text: "Finish the Course" },
     { id: "cg2", text: "Learn all about the course main topic" },
     { id: "cg3", text: "Help other students in the course Q&A" },
-  ];
+  ]);
+
+  const addNewGoalHandler = (newGoal) => {
+    // setCourseGoals(courseGoals.concat(newGoal));
+    setCourseGoals((prevCourseGoals) => prevCourseGoals.concat(newGoal));
+  };
+
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <NewGoal />
+      <NewGoal onAddGoal={addNewGoalHandler} />
       <GoalList goals={courseGoals} />
     </div>
   );
--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./NewGoal.css";
-const NewGoal = () => {
-    const addGoalHandler = event => {
-        event.preventDefault();
 
-        
-    }
+const NewGoal = (props) => {
+  const addGoalHandler = (event) => {
+    event.preventDefault();
+
+    const newGoal = {
+      id: Math.random().toString(),
+      text: "My new goal!",
+    };
+    props.onAddGoal(newGoal);
+  };
   return (
     <form className="new-goal" onSubmit={addGoalHandler}>
       <input type="text" />
