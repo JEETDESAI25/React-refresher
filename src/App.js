3af5e54d7d184eda1b026e42cff3c80bdfeeb423
JEETDESAI25 <jeetdesai25@gmail.com>
2023-02-27 19:06:27 -0800

    Added NewGoal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
-
+import GoalList from "./components/GoalList/GoalList";
+import NewGoal from "./components/NewGoal/NewGoal.js";
 import "./App.css";
 
 const App = () => {
+  const courseGoals = [
+    { id: "cg1", text: "Finish the Course" },
+    { id: "cg2", text: "Learn all about the course main topic" },
+    { id: "cg3", text: "Help other students in the course Q&A" },
+  ];
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <ul className="goal-list">
-        <li>Finish the Course</li>
-        <li>Learn all about the Course main topic</li>
-        <li>Help other students in the course Q&amp;A</li>
-      </ul>
+      <NewGoal />
+      <GoalList goals={courseGoals} />
     </div>
   );
 };
